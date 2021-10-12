156d2622c4a1be3689c1be64ab839b15266de36e
mohamed-mbarek <mbarekmohamed99@gmail.com>
2021-10-11 20:36:46 -0700

    'aaaa'

--- src/app/service/cetegory.service.ts
+++ src/app/service/cetegory.service.ts
@@ -6,26 +6,28 @@ import { Categorie } from "../model/categorie";
 
 @Injectable()
 export class CategorieService {
-    private apiServiceUrl=environment.apiBaseUrl;
+  private apiServiceUrl = environment.apiBaseUrl;
 
+  constructor(private http: HttpClient) {}
 
-constructor(private http:HttpClient){}
-
-
-
-
-public getCategories(): Observable<Categorie[]>{
+  public getCategories(): Observable<Categorie[]> {
     return this.http.get<Categorie[]>(`${this.apiServiceUrl}/categories/all`);
   }
-public addCategorie(categorie :Categorie):Observable<Categorie>{
-return  this.http.post<Categorie>(`${this.apiServiceUrl}/categories/add`,categorie);
-}
-public UpdateCategorie(categorie :Categorie):Observable<Categorie>{
-  return  this.http.put<Categorie>(`${this.apiServiceUrl}/categories/update`,categorie);
-}
-public deleteCategorie(categorieId:number):Observable<void>{
-  return  this.http.delete<void>(`${this.apiServiceUrl}/categories/delete/${categorieId}`);
-  
-}
-
+  public addCategorie(categorie: Categorie): Observable<Categorie> {
+    return this.http.post<Categorie>(
+      `${this.apiServiceUrl}/categories/add`,
+      categorie
+    );
+  }
+  public UpdateCategorie(categorie: Categorie): Observable<Categorie> {
+    return this.http.put<Categorie>(
+      `${this.apiServiceUrl}/categories/update`,
+      categorie
+    );
+  }
+  public deleteCategorie(categorieId: number): Observable<void> {
+    return this.http.delete<void>(
+      `${this.apiServiceUrl}/categories/delete/${categorieId}`
+    );
+  }
 }
